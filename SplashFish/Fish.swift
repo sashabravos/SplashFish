7b0d58c2deb7fba776b1cb6f478625280625c210
Alexandra Kostrova <sashabravos@gmail.com>
2023-09-05 18:28:55 +0300

    Add start settings, add assets

---
+++ SplashFish/Fish.swift
@@ -0,0 +1,8 @@
+//
+//  Fish.swift
+//  SplashFish
+//
+//  Created by Александра Кострова on 05.09.2023.
+//
+
+import Foundation
---
+++ SplashFish/SeaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplashFish
+//
+//  Created by Александра Кострова on 05.09.2023.
+//
+
+import UIKit
+
+class SeaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
